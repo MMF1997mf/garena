01ca2f2bac0ac96376ace0fd60a7a71674418dcf
miyam <miyam@wicresoft.com>
2020-07-05 15:42:00 +0800

    second commit

--- miya-7-5/src/view/anmicition.vue
+++ miya-7-5/src/view/anmicition.vue
@@ -1,28 +1,77 @@
 <template>
 <div class="anmicition-container">
+  <div class="back">
+  </div>
   <div class="translate">
     <!--<img src="../assets/img-1.jpg">-->
   </div>
+  <div class="comment">请使用纵向屏幕观看</div>
 </div>
 </template>
 
 <script>
     export default {
-        name: "anmicition"
+        name: "anmicition",
+      data(){
+          return{
+            isLandscape:null,
+          }
+      },
+      methods:{
+        isPortrait(){
+          if (window.orientation === 180 || window.orientation === 0) {
+            this.isLandscape = false;
+          }
+          if (window.orientation === 90 || window.orientation === -90 ){
+            this.isLandscape = true;
+          }
+        }
+      },
+      mounted(){
+        window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", this.isPortrait(), false);
+      },
+      destroyed(){
+        window.removeEventListener("onorientationchange" in window ? "orientationchange" : "resize",this.isPortrait())
+      },
+      created(){
+
+      }
     }
 </script>
 
 <style scoped>
   .translate{
-    width:400px;
-    height: 400px;
+    width:15rem;
+    height:15rem;
     background: aqua;
     border-radius: 100%;
     background: url(../assets/img-1.jpg);
     background-size: 100% 100%;
     animation:translate 4s linear infinite;
-    margin: 100px auto;
+    margin: 3rem auto;
   }
+  .comment{
+    text-align: center;
+  }
+  @media screen and (min-width:1281px) and (max-width:1920px) and (orientation:portrait) {
+    html { font-size: 15px; }
+  }
+  @media screen and (orientation: portrait) {
+    /*竖屏 css*/
+    .back{
+      display: none;
+    }
+  }
+  @media screen and (orientation: landscape) {
+    .back{
+      height:15rem;
+      width:100%;
+      background: black;
+      opacity: 0.5;
+      position:absolute;
+    }
+  }
+
   @keyframes translate{
     0%{-webkit-transform:rotate(0deg);}
     25%{-webkit-transform:rotate(90deg);}
